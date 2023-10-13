694c14ec231de59205d74dfe918aeb14057abfac
Fugguri <fygguri@icloud.com>
2023-10-13 23:55:20 +0300

    update paths

--- main.py
+++ main.py
@@ -57,5 +57,5 @@ app.include_router(crm_router)
 app.include_router(chat_router)
 
 if __name__ == "__main__":
-    create_api()
+    create_api(app)
     uvicorn.run(app, host='0.0.0.0', port=8000)
