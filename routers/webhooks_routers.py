068e0ba239283955729a4737452bd89d8bceb10c
Fugguri <fygguri@gmail.com>
2023-11-06 09:45:18 +0300

    update

--- routers/webhooks_routers.py
+++ routers/webhooks_routers.py
@@ -9,7 +9,7 @@ from mics.tg import TgBot
 webhooks_router = APIRouter()
 
 
-@webhooks_router.post("/webhook/tg_bot/{bot_id}", response_model=models.Profile, name="Данные профиля", description="Данные по пользователе", tags=["Профиль"])
+@webhooks_router.post("/webhook/tg_bot/{bot_id}",  name="Получение сообщения", description="", tags=["webhooks"])
 async def profile(bot_id: str, request: Request):
     tg_bot = await prisma.telegrambot.find_first(where={"id": bot_id})
     tg_provider: TgBot = TgBot(token=tg_bot.token)
@@ -19,4 +19,4 @@ async def profile(bot_id: str, request: Request):
     print(message.get("text"))
     tg_provider.bot.send_message(message.chat.id, message.text)
     # profile = await utls.check_profile_access_token(access_token, False)
-    return profile
+    # return profile
