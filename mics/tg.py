009698e363b230f7b6854025bff56d2fb4ade9ee
Fugguri <fygguri@gmail.com>
2023-11-06 09:37:36 +0300

    update

--- mics/tg.py
+++ mics/tg.py
@@ -1,27 +1,28 @@
-from aiogram import Bot, Dispatcher,executor, types
+from aiogram import Bot, Dispatcher, executor, types
 
 import asyncio
- 
+
+
 class TgBot:
-    
-    def __init__(self,token=None) -> None:
-        
-        self.bot:Bot = Bot(token=token)
-        self.dispather:Dispatcher = Dispatcher(bot=self.bot) 
+
+    def __init__(self, token=None) -> None:
+
+        self.bot: Bot = Bot(token=token)
+        self.dispather: Dispatcher = Dispatcher(bot=self.bot)
         self.executor = executor
-        
-    async def getInfo(self) -> [types.User,str]:
+        self.types: types = types
+
+    async def getInfo(self) -> [types.User, str]:
         self.dispather.start_polling()
-        
+
         me = await self.bot.get_me()
-        pict =await self.bot.get_user_profile_photos(me.id)
+        pict = await self.bot.get_user_profile_photos(me.id)
         file = await self.bot.get_file(file_id=pict.photos[0][0].file_id)
         url = self.bot.get_file_url(file_path=file)
         self.dispather.stop_polling()
 
         return [me, url]
-        
-    
+
+
 class TgUserBot:
     pass
-
--- routers/webhooks_routers.py
+++ routers/webhooks_routers.py
@@ -3,15 +3,18 @@ from models import schemas
 from prisma_ import prisma
 from prisma import models
 from mics import utls
+from mics.tg import TgBot
 
 
 webhooks_router = APIRouter()
 
 
-@webhooks_router.post("/webhook/tg_bot/{access_token}", response_model=models.Profile, name="Данные профиля", description="Данные по пользователе", tags=["Профиль"])
-async def profile(access_token: str, request: Request):
-    message = await request.json()
+@webhooks_router.post("/webhook/tg_bot/{bot_id}", response_model=models.Profile, name="Данные профиля", description="Данные по пользователе", tags=["Профиль"])
+async def profile(bot_id: str, request: Request):
+    tg_bot = await prisma.telegrambot.find_first(where={"id": bot_id})
+    tg_provider: TgBot = TgBot(token=tg_bot.token)
 
-    print(body)
-    profile = await utls.check_profile_access_token(access_token, False)
+    message = TgBot.types.Message(await request.json())
+    tg_provider.bot.send_message(message.chat.id, message.text)
+    # profile = await utls.check_profile_access_token(access_token, False)
     return profile
