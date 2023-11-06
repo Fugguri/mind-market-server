2c9f7428346d02adf12736eccecd4749dc41c590
Fugguri <fygguri@gmail.com>
2023-11-06 09:51:42 +0300

    update

--- routers/webhooks_routers.py
+++ routers/webhooks_routers.py
@@ -4,6 +4,7 @@ from prisma_ import prisma
 from prisma import models
 from mics import utls
 from mics.tg import TgBot
+from aiogram.types import Message
 
 
 webhooks_router = APIRouter()
@@ -15,10 +16,10 @@ async def profile(bot_id: str, request: Request):
     tg_provider: TgBot = TgBot(token=tg_bot.token)
 
     data = await request.json()
-    print(data)
-    message = data.get("message")
+    print
+    message = Message(data)
     print(message)
-    print(message.get("text"))
+    print(message.text)
     tg_provider.bot.send_message(message.chat.id, message.text)
     # profile = await utls.check_profile_access_token(access_token, False)
     # return profile
