efd01255de029ce4f7e0a7505539905946626e5b
Fugguri <fygguri@gmail.com>
2023-11-18 19:37:01 +0300

    update

--- mics/_openai.py
+++ mics/_openai.py
@@ -1,17 +1,25 @@
-import openai
+import httpx
+from openai import OpenAI
 from dotenv import dotenv_values
+config = dotenv_values(".env")
+
+openai = OpenAI(
+    api_key=config['openAi'],
+
+    http_client=httpx.Client(
+        proxies="http://9gfWr9:g0LSUy@131.108.17.194:9799/",
+        transport=httpx.HTTPTransport(local_address="0.0.0.0"),
+    ),
+)
+
 users_message = {}
 
 
-config = dotenv_values(".env")
-openai.api_key = config['openAi']
-
-
 async def create_responce(user_id: int | str, settings: str, text: str | int):
-    proxy = {
-        "http": "http://9gfWr9:g0LSUy@131.108.17.194:9799/",
-        "https": "http://9gfWr9:g0LSUy@131.108.17.194:9799/"
-    }
+    # proxy = {
+    #     "http": "http://9gfWr9:g0LSUy@131.108.17.194:9799/",
+    #     "https": "http://9gfWr9:g0LSUy@131.108.17.194:9799/"
+    # }
     try:
         answer = ""
         try:
@@ -20,11 +28,10 @@ async def create_responce(user_id: int | str, settings: str, text: str | int):
             users_message[user_id] = [{"role": "user", "content": settings}]
         users_message[user_id].append(
             {"role": "user", "content": text})
-        responce = openai.ChatCompletion.create(
+        responce = openai.chat.completions.create(
             model="gpt-4-1106-preview",
             messages=users_message[user_id],
             temperature=0.7,
-            proxy=proxy
         )
         answer = responce['choices'][0]['message']['content']
 
