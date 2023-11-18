33556d6b68fcb3bc16bbec0b5ae06d5d897140df
Fugguri <fygguri@gmail.com>
2023-11-18 19:27:28 +0300

    update

--- mics/_openai.py
+++ mics/_openai.py
@@ -8,7 +8,10 @@ openai.api_key = config['openAi']
 
 
 async def create_responce(user_id: int | str, settings: str, text: str | int):
-
+    proxy = {
+        "http": "http://9gfWr9:g0LSUy@131.108.17.194:9799/",
+        "https": "http://9gfWr9:g0LSUy@131.108.17.194:9799/"
+    }
     try:
         answer = ""
         try:
@@ -21,7 +24,7 @@ async def create_responce(user_id: int | str, settings: str, text: str | int):
             model="gpt-4-1106-preview",
             messages=users_message[user_id],
             temperature=0.7,
-            proxy='http://9gfWr9:g0LSUy@131.108.17.194:9799/'
+            proxy=proxy
         )
         answer = responce['choices'][0]['message']['content']
 
