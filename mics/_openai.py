c1c4480fcff55537bebd136fb3bc98e9109e8cb9
Fugguri <fygguri@gmail.com>
2023-11-18 20:16:34 +0300

    update

--- mics/_openai.py
+++ mics/_openai.py
@@ -1,13 +1,14 @@
 import httpx
+from openai import RateLimitError
 from openai import OpenAI
 from dotenv import dotenv_values
 config = dotenv_values(".env")
-
+proxy = config["proxy"],
 openai = OpenAI(
     api_key=config['openAi'],
 
     http_client=httpx.Client(
-        proxies="http://9gfWr9:g0LSUy@131.108.17.194:9799/",
+        proxies=proxy,
         transport=httpx.HTTPTransport(local_address="0.0.0.0"),
     ),
 )
@@ -16,30 +17,32 @@ users_message = {}
 
 
 async def create_responce(user_id: int | str, settings: str, text: str | int):
-    # proxy = {
-    #     "http": "http://9gfWr9:g0LSUy@131.108.17.194:9799/",
-    #     "https": "http://9gfWr9:g0LSUy@131.108.17.194:9799/"
-    # }
     try:
         answer = ""
-        try:
-            users_message[user_id]
-        except:
+        users_message.get(user_id)
+        if not users_message:
             users_message[user_id] = [{"role": "user", "content": settings}]
-        users_message[user_id].append(
-            {"role": "user", "content": text})
-        responce = openai.chat.completions.create(
+            users_message[user_id].append(
+                {"role": "user", "content": text})
+        current_settings = users_message[user_id][0].get("content")
+        if settings != current_settings:
+            users_message[user_id] = [{"role": "user", "content": settings}]
+            users_message[user_id].append({"role": "user", "content": text})
+
+        response = openai.chat.completions.create(
             model="gpt-4-1106-preview",
             messages=users_message[user_id],
             temperature=0.7,
+            max_tokens=100
         )
-        answer = responce.choices[0].message.content
+
+        answer = response.choices[0].message.content
 
         users_message[user_id].append({"role": "assistant", "content": answer})
-    except openai.error.RateLimitError as ex:
+    except RateLimitError as ex:
         return "RateLimitError"
     except Exception as ex:
-        print(ex)
         users_message[user_id] = []
         create_responce(user_id, text)
+
     return answer
