174971125a510a3da781cfeec0f3b1de5aebbbfa
Fugguri <fygguri@gmail.com>
2023-11-22 16:55:18 +0300

    update

--- routers/webhooks_routers.py
+++ routers/webhooks_routers.py
@@ -12,17 +12,19 @@ webhooks_router = APIRouter()
 
 @webhooks_router.post("/webhook/tg_bot/{bot_id}",  name="Получение сообщения", description="", tags=["webhooks"])
 async def profile(bot_id: str, request: Request):
-    tg_bot = await prisma.telegrambot.find_first(where={"id": bot_id}, include={"assistant": True})
-    tg_provider: TgBot = TgBot(token=tg_bot.token)
+    print(request)
 
-    data = await request.json()
+    # tg_bot = await prisma.telegrambot.find_first(where={"id": bot_id}, include={"assistant": True})
+    # tg_provider: TgBot = TgBot(token=tg_bot.token)
 
-    message = data.get("message")
-    user_id = message.get("from").get("id")
-    text = message.get("text")
-    response = await _openai.create_responce(
-        user_id, tg_bot.assistant.settings, text)
+    # data = await request.json()
 
-    await tg_provider.bot.send_message(user_id, response)
-    # profile = await utls.check_profile_access_token(access_token, False)
-    # return profile
+    # message = data.get("message")
+    # user_id = message.get("from").get("id")
+    # text = message.get("text")
+    # response = await _openai.create_responce(
+    #     user_id, tg_bot.assistant.settings, text)
+
+    # await tg_provider.bot.send_message(user_id, response)
+    # # profile = await utls.check_profile_access_token(access_token, False)
+    # # return profile
