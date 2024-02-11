89e794fd55ed3257d53cba6810ddab3892716f35
Fugguri <fygguri@gmail.com>
2024-02-12 00:29:44 +0300

    update

--- mics/jivo.py
+++ mics/jivo.py
@@ -1,10 +1,9 @@
 import requests
 from models import schemas
 from mics._openai import create_response
-from prisma import models
 
 
-async def create_jivo_responce(request: schemas.ClientMessage, assistant: models.Assistant) -> schemas.BotMessage:
+async def create_jivo_responce(request: schemas.ClientMessage, assistant) -> schemas.BotMessage:
 
     response = schemas.BotMessage(**request.dict())
     response.event = "BOT_MESSAGE"
@@ -13,7 +12,7 @@ async def create_jivo_responce(request: schemas.ClientMessage, assistant: models
     return response
 
 
-async def send_jivo_aswer(response: schemas.BotMessage, assistant: models.Assistant):
+async def send_jivo_aswer(response: schemas.BotMessage, assistant):
 
     url = f"https://bot.jivosite.com/webhooks/{assistant.jivoBot[0].provider_id}/{assistant.token}"
     json = response.dict()
