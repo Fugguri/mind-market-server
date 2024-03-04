a8ba8fa303ec811986d952225a1d24fc1f41f3f6
Fugguri <fygguri@gmail.com>
2024-03-04 20:01:51 +0300

    update

--- routers/webhooks_routers.py
+++ routers/webhooks_routers.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Path, Query, Request, HTTPException
-
+from aiogram import types
 from mics import utls, _openai
 from mics.tg import TgBot
 from aiogram.types import Message
@@ -9,16 +9,7 @@ webhooks_router = APIRouter()
 
 
 @webhooks_router.post("/webhooks/tgbot/{bot_id}",  name="Получение сообщения от телеграмм", description="", tags=["webhooks"])
-async def profile(bot_id: str, request: Request):
+async def profile(bot_id: str, message: types.Message):
     print(bot_id)
-    req = await request.json()
-    print(req)
-    message = req.get("message")
-    if not message:
-        return
-    sender = message.get("from")
-    chat = message.get("chat")
-    text = message.get("text")
-    if text:
-        print(text)
-    create_response(user_id="", settings="", text=text)
+    print(message)
+    # create_response(user_id="", settings="", text=text)
