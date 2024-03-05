39004c8039ee4db41ace91818df8b67ca4c918ab
Fugguri <fygguri@gmail.com>
2024-03-05 05:40:38 +0300

    update

--- routers/webhooks_routers.py
+++ routers/webhooks_routers.py
@@ -4,15 +4,16 @@ from mics import utls, _openai
 from mics.tg import TgBot
 from aiogram.types import Message
 from mics._openai import create_response
-
+from DB.db import AsyncSession, get_session
+from DB.async_crud import get_tg_bot
 webhooks_router = APIRouter()
 
 bots = dict()
 
 
 @webhooks_router.post("/webhooks/tgbot/{bot_id}",  name="Получение сообщения от телеграмм", description="", tags=["webhooks"])
-async def profile(bot_id: str, request: Request):
-
+async def profile(bot_id: str, request: Request, session: AsyncSession = Depends(get_session)):
+    print(await get_tg_bot(bot_id))
     req = await request.json()
     print(req)
     message = req.get("message")
