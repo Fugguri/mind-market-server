a28639dbbdc9793fc1f14e46ff7d72f04286620f
Fugguri <fygguri@gmail.com>
2024-03-05 05:44:44 +0300

    update

--- DB/async_crud.py
+++ DB/async_crud.py
@@ -40,7 +40,6 @@ async def add_tg_bot(session: AsyncSession,
     new_bot.first_name = first_name
     new_bot.username = username
     new_bot.startMessage = startMessage
-    new_bot.createdAt = datetime.datetime.now()
-    new_bot.updatedAt = datetime.datetime.now()
+    print(new_bot.startMessage)
     session.add(new_bot)
     return new_bot
