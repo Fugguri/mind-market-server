987b06cb9d2ed8a13f2a170ac82c6fb3bf1cfe02
Fugguri <fygguri@gmail.com>
2024-03-05 06:04:09 +0300

    update

--- DB/async_crud.py
+++ DB/async_crud.py
@@ -18,7 +18,7 @@ async def checker(func):
 
 # @checker
 async def get_tg_bot(session: AsyncSession, bot_id) -> TelegramBot:
-    result = await session.execute(select(TelegramBot).filter(id=bot_id))
+    result = await session.execute(select(TelegramBot).filter(TelegramBot.id == bot_id))
     return result.fetchone()
 
 
