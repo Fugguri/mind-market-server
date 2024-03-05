4c4da69c526190ccecf6b959a7d4c4c019863ec5
Fugguri <fygguri@gmail.com>
2024-03-05 05:28:15 +0300

    update

--- routers/integration_router.py
+++ routers/integration_router.py
@@ -21,7 +21,7 @@ async def create_tg_bot(projectId: str, request: Request, session: AsyncSession 
 @integration_router.post("/integrations/tgbot", name="Добавление Telegram бота", description="Добавление бота, созданного в BotFather ", tags=["Интеграции"])
 async def create_tg_bot(tgbot: schemas.TgBotEntry, session: AsyncSession = Depends(get_session)):
     bot = tg.TgBot(tgbot.botToken)
-    print(tgbot.botToken, tgbot.assistantId, tgbot.projectId)
+    print(tgbot.botToken)
     try:
         info = await bot.getInfo()
         me: types.User = info[0]
