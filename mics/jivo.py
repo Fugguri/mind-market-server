f3b47d4c1248fb06090a6273cf707c48065e51c9
Fugguri <fygguri@gmail.com>
2024-03-12 18:44:58 +0300

    update

--- mics/jivo.py
+++ mics/jivo.py
@@ -19,7 +19,7 @@ async def send_jivo_aswer(response: schemas.BotMessage, provider_id: str, ptojec
 
     headers = {"Content-Type": "application/json"}
     result = requests.post(url=url, headers=headers, json=json)
-    print(result.url)
+    print(json)
     print(result.request.body)
     print(result.status_code)
     print(result.content)
