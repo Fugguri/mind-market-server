26c96ba9d77eb70cbf047c1644c2be2338e1f492
Fugguri <fygguri@gmail.com>
2024-03-19 09:49:11 +0300

    update

--- mics/jivo.py
+++ mics/jivo.py
@@ -1,4 +1,4 @@
-import httpx
+import requests
 from models import schemas
 from mics._openai import create_response
 
@@ -12,15 +12,15 @@ async def create_jivo_responce(request: schemas.ClientMessage, assistant) -> sch
     return response
 
 
-async def send_jivo_aswer(response_model: schemas.BotMessage, provider_id: str, ptoject_id: str):
+async def send_jivo_aswer(response: schemas.BotMessage, provider_id: str, ptoject_id: str):
 
     url = f"https://bot.jivosite.com/webhooks/{provider_id}/{ptoject_id}"
-    json = response_model.model_dump_json()
+    json = response.model_dump_json()
 
     headers = {"Content-Type": "application/json"}
-    async with httpx.AsyncClient() as client:
-        response = await client.post(url=url, headers=headers, json=json)
-        # print(json)
-        print(response.status_code)
-        print(response.content)
-    return response
+    result = requests.post(url=url, headers=headers, data=json)
+    print(json)
+    print(result.request.body)
+    print(result.status_code)
+    print(result.content)
+    return result
