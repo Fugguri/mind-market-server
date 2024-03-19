3bec6730bed78b48b88e23b58bb83a452f0fca72
Fugguri <fygguri@gmail.com>
2024-03-19 10:07:24 +0300

    update

--- mics/jivo.py
+++ mics/jivo.py
@@ -26,21 +26,3 @@ async def send_jivo_aswer(response: schemas.BotMessage, provider_id: str, ptojec
         print(result.status_code)
         print(result.content)
     return result
-
-
-{"id": "996344da-d8f4-11ec-9aaf-c5bde7170335",
-    "event": "BOT_MESSAGE",
-    "message":
-    {"type": "TEXT",
-     "text": "Конечно, я готов вам помочь. Какой у вас вопрос?",
-     "timestamp": 1653127681833},
-    "client_id": "3209", "chat_id": "4209"
- }
-{"id": "2bc2f2c2-e5b8-11ee-91b4-b758e91cf880",
-    "event": "BOT_MESSAGE"
-    "message": 
-    {"type": "TEXT", 
-     "text": "Привет! Как я могу помочь тебе с услугами по внедрению искусственного интеллекта?", 
-     "timestamp": 1710828966}, 
-    "client_id": "2", "chat_id": "28", 
- }
