c56cc126bda56c456fa59d552f9d00e6368b5909
Fugguri <fygguri@gmail.com>
2024-04-28 22:17:28 +0300

    update

--- routers/integration_router.py
+++ routers/integration_router.py
@@ -145,7 +145,8 @@ async def create_user(project_id: str, request: schemas.ClientMessage, session: 
 
 def run_async(project_id: str, request: schemas.ClientMessage, session: AsyncSession = Depends(get_session)):
     loop = asyncio.get_running_loop()
-    asyncio.run(create_jivo_answer(project_id, request, session))
+    asyncio.run_coroutine_threadsafe(create_jivo_answer(
+        project_id, request, session), loop=loop)
 
 
 async def create_jivo_answer(project_id: str, request: schemas.ClientMessage, session: AsyncSession = Depends(get_session)):
